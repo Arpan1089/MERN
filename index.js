9737c566423210b177ff4e00a2ae7bb1a923e365
arpan1089 <git>
2018-02-25 12:29:53 +0530

    First commit in MERN

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+var app = express();
+app.get('/',(req,res) =>{
+  
+    res.send('hi');
+});
+
+//dynamic port binding 
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
